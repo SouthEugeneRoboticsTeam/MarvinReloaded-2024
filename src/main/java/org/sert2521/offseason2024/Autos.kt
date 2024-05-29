c02d0c56f646a438c14330b87aca2a9226de519f
Kona <conradulysses@outlook.com>
2024-05-28 17:49:38 -0700

    Setup

--- src/main/java/org/sert2521/offseason2024/Autos.kt
+++ src/main/java/org/sert2521/offseason2024/Autos.kt
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.PrintCommand
-import org.sert2521.offseason2024.subsystems.ExampleSubsystem
+
 
 object Autos
 {
---
+++ src/main/java/org/sert2521/offseason2024/subsystems/Example.kt
@@ -0,0 +1,4 @@
+package org.sert2521.offseason2024.subsystems
+
+object Example {
+}
