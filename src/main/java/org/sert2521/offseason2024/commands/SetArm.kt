80525be566a24817948ed83b999391fa7851d737
Kona-Watson <watsonkona405@gmail.com>
2024-06-14 14:57:54 -0700

    Started work on SetArm.kt :3

--- src/main/java/org/sert2521/offseason2024/commands/SetArm.kt
+++ src/main/java/org/sert2521/offseason2024/commands/SetArm.kt
@@ -2,44 +2,20 @@ package org.sert2521.offseason2024.commands
 
 import edu.wpi.first.math.controller.ArmFeedforward
 import edu.wpi.first.math.controller.PIDController
-import edu.wpi.first.math.controller.ProfiledPIDController
 import edu.wpi.first.wpilibj2.command.Command
-import org.sert2521.offseason2024.ConfigConsts
 import org.sert2521.offseason2024.PIDFFConsts
 import org.sert2521.offseason2024.subsystems.arm.Arm
 import org.sert2521.offseason2024.subsystems.arm.ArmIO
 import org.sert2521.offseason2024.subsystems.arm.ArmIOSparkMax
-import kotlin.math.abs
 
-class SetArm (private val goal:Double, private val ends:Boolean = true) : Command() {
-    private var armAngle = 0.0
+class SetArm (private val goal:Double, private val ende:Boolean = true) : Command() {
+    private val armAngle = Arm.io.getRadians()
     private val feedForward = ArmFeedforward(PIDFFConsts.ARM_S, PIDFFConsts.ARM_G, PIDFFConsts.ARM_G, PIDFFConsts.ARM_A)
-    private val pid = ProfiledPIDController(PIDFFConsts.ARM_P, PIDFFConsts.ARM_I, PIDFFConsts.ARM_D, PIDFFConsts.trapConstraints)
-
-    private var pidOutput = 0.0
-    private var feedForwardOutput = 0.0
+    private val pid = PIDController(PIDFFConsts.ARM_P, PIDFFConsts.ARM_I, PIDFFConsts.ARM_D)
 
     private var done = false
 
     init {
         addRequirements(Arm)
-    }
-
-    override fun execute() {
-        done = false
-
-        armAngle = Arm.io.getRadians()
-        pidOutput = pid.calculate(armAngle, goal)
-        feedForwardOutput = feedForward.calculate(armAngle, pid.setpoint.velocity)
-
-        Arm.io.setVoltage(pidOutput + feedForwardOutput)
-
-        if((abs(goal - armAngle)) <= ConfigConsts.ARM_ANGLE_TOLERANCE_RAD){
-            done = true
-        }
-    }
-
-    override fun isFinished(): Boolean {
-        return true
     }
 }
