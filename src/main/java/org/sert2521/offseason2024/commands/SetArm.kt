cf5a73a86d62b5c7d2cd808c5f8f953960fc2945
Kona-Watson <watsonkona405@gmail.com>
2024-06-18 16:20:10 -0700

    Idk :3

--- src/main/java/org/sert2521/offseason2024/commands/SetArm.kt
+++ src/main/java/org/sert2521/offseason2024/commands/SetArm.kt
@@ -9,13 +9,24 @@ import org.sert2521.offseason2024.subsystems.arm.ArmIO
 import org.sert2521.offseason2024.subsystems.arm.ArmIOSparkMax
 
 class SetArm (private val goal:Double, private val ende:Boolean = true) : Command() {
-    private val armAngle = Arm.io.getRadians()
+    private var armAngle = 0.0
     private val feedForward = ArmFeedforward(PIDFFConsts.ARM_S, PIDFFConsts.ARM_G, PIDFFConsts.ARM_G, PIDFFConsts.ARM_A)
     private val pid = PIDController(PIDFFConsts.ARM_P, PIDFFConsts.ARM_I, PIDFFConsts.ARM_D)
+
+    private var pidOutput = 0.0
+    private var feedForwardOutput = 0.0
 
     private var done = false
 
     init {
         addRequirements(Arm)
+    }
+
+    override fun execute() {
+        armAngle = Arm.io.getRadians()
+        pidOutput = pid.calculate(armAngle, goal)
+
+        //Add this
+        //feedForwardOutput = feedForward.calculate(armAngle,)
     }
 }
