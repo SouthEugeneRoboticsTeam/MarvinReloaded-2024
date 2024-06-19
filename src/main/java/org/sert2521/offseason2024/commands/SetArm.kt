c2ca2cdca27a940fb15129d02f0985b658f3f66d
Kona-Watson <watsonkona405@gmail.com>
2024-06-18 17:00:21 -0700

    Added function to get arm encoder angle ',:3c

--- src/main/java/org/sert2521/offseason2024/commands/SetArm.kt
+++ src/main/java/org/sert2521/offseason2024/commands/SetArm.kt
@@ -8,7 +8,7 @@ import org.sert2521.offseason2024.subsystems.arm.Arm
 import org.sert2521.offseason2024.subsystems.arm.ArmIO
 import org.sert2521.offseason2024.subsystems.arm.ArmIOSparkMax
 
-class SetArm (private val goal:Double, private val ende:Boolean = true) : Command() {
+class SetArm (private val goal:Double, private val ends:Boolean = true) : Command() {
     private var armAngle = 0.0
     private val feedForward = ArmFeedforward(PIDFFConsts.ARM_S, PIDFFConsts.ARM_G, PIDFFConsts.ARM_G, PIDFFConsts.ARM_A)
     private val pid = PIDController(PIDFFConsts.ARM_P, PIDFFConsts.ARM_I, PIDFFConsts.ARM_D)
@@ -25,8 +25,6 @@ class SetArm (private val goal:Double, private val ende:Boolean = true) : Comman
     override fun execute() {
         armAngle = Arm.io.getRadians()
         pidOutput = pid.calculate(armAngle, goal)
-
-        //Add this
-        //feedForwardOutput = feedForward.calculate(armAngle,)
+        feedForwardOutput = feedForward.calculate(armAngle, pid.setpoint) //This is probably wrong
     }
 }
